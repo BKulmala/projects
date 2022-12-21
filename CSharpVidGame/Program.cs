101eaae883afba16ab9c89bc869c846f40eac1b2
BKulmala <bjk74@uakron.edu>
2022-12-21 17:20:35 -0500

    Hangman

---
+++ CSharpVidGame/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true) {
+            char[] threeStrikes = new char[] {'O','O','O'};
+            string wordGuess;
+            Console.WriteLine("( |  _  | )\n");
+            Console.WriteLine("     |     \n");
+            Console.WriteLine("-----|-----\n");
+            Console.WriteLine("     |     \n");
+            Console.WriteLine("    /-\\   \n"); 
+            Console.WriteLine(threeStrikes[0] + "    " + threeStrikes[1] + "     " + threeStrikes[2]);
+            wordGuess = Console.ReadLine();
+            Console.Clear();
+            } 
+        }
+    }
+}
