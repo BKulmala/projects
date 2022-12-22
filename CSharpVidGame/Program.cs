fb0e4ff9ca38529ef6764012ae8cf4bdc3f829ff
BKulmala <bjk74@uakron.edu>
2022-12-21 22:33:38 -0500

    Hangman video game

--- CSharpVidGame/Program.cs
+++ CSharpVidGame/Program.cs
@@ -6,17 +6,32 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            while(true) {
+            int chances = 3;
+            char[] wordTest = new char[] {'T','e','s','t'};
+            char[] underlineFill = new char[] {'_','_','_','_'};
             char[] threeStrikes = new char[] {'O','O','O'};
-            string wordGuess;
+            while(!underlineFill.SequenceEqual(wordTest)) {
+            char letterGuess;
             Console.WriteLine("( |  _  | )\n");
             Console.WriteLine("     |     \n");
             Console.WriteLine("-----|-----\n");
             Console.WriteLine("     |     \n");
             Console.WriteLine("    /-\\   \n"); 
             Console.WriteLine(threeStrikes[0] + "    " + threeStrikes[1] + "     " + threeStrikes[2]);
-            wordGuess = Console.ReadLine();
+            Console.WriteLine(underlineFill);
+            letterGuess = Console.ReadLine()[0];
             Console.Clear();
+            if(wordTest.Contains(letterGuess)) {
+                for(int i = 0;i != wordTest.Length;i++) {
+                    if(wordTest[i] == letterGuess) {
+                        underlineFill[i] = letterGuess;
+                    }
+                }
+            }
+            else{
+                --chances;
+                threeStrikes[chances] = 'X';
+            }
             } 
         }
     }
